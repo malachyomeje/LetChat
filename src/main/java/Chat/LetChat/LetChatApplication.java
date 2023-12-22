32a54e34ec4dd3c25d256631339033efec3f4430
malachy <malachyomeje@gmail.com>
2023-12-22 18:51:32 +0100

    first commit

---
+++ src/main/java/Chat/LetChat/LetChatApplication.java
@@ -0,0 +1,13 @@
+package Chat.LetChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Chat/LetChat/LetChatApplicationTests.java
@@ -0,0 +1,13 @@
+package Chat.LetChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
